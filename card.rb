a7ec3f81d66e43b1650733083e70396bfc59cbb4
fizzleRaider7645 <douglasuretsky@gmail.com>
2018-09-30 14:27:06 -0400

    create generate deck method

--- card.rb
+++ card.rb
@@ -1,2 +1,23 @@
 class Card
+
+  def initialize(suit, value)
+    @suit = suit
+    @value = value
+  end
+
+  VALUES = [1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+  SUITS = [:H, :S, :D, :C]
+
+  def self.generate_deck
+    deck = []
+    SUITS.each do |suit|
+      VALUES.each do |value|
+        deck << Card.new(suit, value)
+      end
+    end
+    deck
+  end
+
+
+
 end
